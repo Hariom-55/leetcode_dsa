789ac6b58faa3b3baf69cd01f80b9b0b53e25192
Hariom Rajkumar Tiwari <hariomr2204t@gmail.com>
2026-07-23 22:09:08 +0530

    Time: 0 ms (100%), Space: 11 MB (19.5%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int find(vector<int> &a){
         int mxc=-1;
-        for(int i=0;i<256;i++){
+        for(int i=0;i<26;i++){
             mxc=max(mxc,a[i]);
         }
         return mxc;
@@ -12,14 +12,14 @@ public:
         int n = s.length();
         int low=0;
         int res=INT_MIN;
-        vector<int> freq(256,0);
+        vector<int> freq(26,0);
         for(int high=0;high<n;high++){
-            freq[s[high]]++;
+            freq[s[high]-'A']++;
             int len =high-low+1;
             int max_Count=find(freq);
             int diff =len-max_Count;
             while(diff > k){
-                freq[s[low]]--;
+                freq[s[low]-'A']--;
                 low++;
                 len =high-low+1;
                 max_Count=find(freq);
